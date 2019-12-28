ed8fa4d04ff90407bfcd51696d6319fcf2c2793c
avikola <avishkar1995@gmail.com>
2019-12-28 04:44:48 -0800

    Don't Press Me!

---
+++ src/components/DontPressMe.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Toast from "react-bootstrap/Toast";
+import "./DontPressMe.scss";
+
+export const DontPressMe = props => {
+  return (
+    <Toast
+      style={{
+        position: "fixed",
+        marginTop: -40
+      }}
+      onClose={props.closeit}
+      className="dontpressmetoast"
+      show={props.toast_show}
+      delay={3000}
+      autohide
+    >
+      <Toast.Body>{props.press_dictionary[props.num_of_clicks]}</Toast.Body>
+    </Toast>
+  );
+};
--- src/routes/App/Intro.js
+++ src/routes/App/Intro.js
@@ -1,12 +1,90 @@
 import React from "react";
 import "./Intro.scss";
+import { DontPressMe } from "../../components/DontPressMe";
 
-export const Intro = () => {
-  return (
-    <div className="intro-text">
-      <p className="p-hello">Hello, I'm</p>
-      <h1>Avishkar Kolahalu.</h1>
-      <p className="p-sd">&#47;&#47; Software Developer</p>
-    </div>
-  );
-};
+export class Intro extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num_of_clicks: 0,
+      toast_show: false,
+      dict_check: 1
+    };
+    this.dontPressMe = this.dontPressMe.bind(this);
+    this.press_dictionary = [
+      "",
+      "Hello there.",
+      "You can call me Avi.",
+      "This is not the button you're looking for",
+      "Click somewhere else!",
+      "No.",
+      "Fine, you win.",
+      "Click again for a prize."
+    ];
+  }
+
+  dontPressMe() {
+    if (this.state.num_of_clicks < 7) {
+      if (this.state.num_of_clicks + 1 === 7)
+        this.setState({
+          num_of_clicks: this.state.num_of_clicks + 1,
+          toast_show: true,
+          dict_check: 2
+        });
+      else
+        this.setState({
+          num_of_clicks: this.state.num_of_clicks + 1,
+          toast_show: true
+        });
+    }
+
+    if (this.state.num_of_clicks === 8) this.setState({ dict_check: 3 });
+  }
+
+  closeToast() {
+    this.setState({
+      toast_show: false
+    });
+  }
+
+  render() {
+    return (
+      <div className="intro-text">
+        <DontPressMe
+          closeit={this.closeToast.bind(this)}
+          toast_show={this.state.toast_show}
+          press_dictionary={this.press_dictionary}
+          num_of_clicks={this.state.num_of_clicks}
+        />
+
+        <p className="p-hello">Hello, I'm</p>
+
+        {this.state.dict_check === 1 && (
+          <h1 onClick={this.dontPressMe}>Avishkar Kolahalu.</h1>
+        )}
+
+        {this.state.dict_check === 2 && (
+          <h1>
+            <a
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rolled-link"
+              onClick={() =>
+                this.setState({
+                  dict_check: 3
+                })
+              }
+            >
+              Avishkar Kolahalu.
+            </a>
+          </h1>
+        )}
+
+        {this.state.dict_check === 3 && <h1>Avishkar Kolahalu.</h1>}
+
+        <p className="p-sd">&#47;&#47; Software Developer</p>
+      </div>
+    );
+  }
+}
