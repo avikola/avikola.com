dfd727572beded1ef5ab117974f24ab72f27952d
avikola <avishkar1995@gmail.com>
2019-12-28 13:52:12 -0800

    confetti!

--- src/components/DontPressMe.js
+++ src/components/DontPressMe.js
@@ -3,13 +3,14 @@ import Toast from "react-bootstrap/Toast";
 import "./DontPressMe.scss";
 
 export const DontPressMe = props => {
+  let delay_secs = 3000;
+  if (props.num_of_clicks == 10) delay_secs = 6000;
   return (
     <Toast
-      style={{}}
       onClose={props.closeit}
       className="dontpressmetoast"
       show={props.toast_show}
-      delay={3000}
+      delay={delay_secs}
       autohide
     >
       <Toast.Body>{props.press_dictionary[props.num_of_clicks]}</Toast.Body>
--- src/routes/App/Intro.js
+++ src/routes/App/Intro.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Intro.scss";
 import { DontPressMe } from "../../components/DontPressMe";
+import Confetti from "react-confetti";
 
 export class Intro extends React.Component {
   constructor(props) {
@@ -8,37 +9,46 @@ export class Intro extends React.Component {
     this.state = {
       num_of_clicks: 0,
       toast_show: false,
-      dict_check: 1
+      dict_check: 1,
+      confetti: false,
+      gravity: 0.8
     };
     this.dontPressMe = this.dontPressMe.bind(this);
+    this.startConfetti = this.startConfetti.bind(this);
     this.press_dictionary = [
       "",
       "Hello there.",
-      "You can call me Avi.",
-      "Yeah, this isn't a button.",
+      "You can call me Avi!",
+      "Hey, this isn't a button.",
       "Click somewhere else!",
       "No. Stop.",
       "Fine, you win.",
-      "Click again for the prize!"
+      "I baked you cake.",
+      "It's delicious, I promise.",
+      "Initiating surprise in 3..2..1..",
+      "I made it all up."
     ];
   }
 
   dontPressMe() {
-    if (this.state.num_of_clicks < 7) {
-      if (this.state.num_of_clicks + 1 === 7)
-        this.setState({
-          num_of_clicks: this.state.num_of_clicks + 1,
-          toast_show: true,
-          dict_check: 2
-        });
-      else
-        this.setState({
-          num_of_clicks: this.state.num_of_clicks + 1,
-          toast_show: true
-        });
-    }
+    if (this.state.num_of_clicks < 8)
+      this.setState({
+        num_of_clicks: this.state.num_of_clicks + 1,
+        toast_show: true
+      });
 
-    if (this.state.num_of_clicks === 8) this.setState({ dict_check: 3 });
+    if (this.state.num_of_clicks === 8)
+      this.setState({
+        dict_check: 2,
+        num_of_clicks: this.state.num_of_clicks + 1,
+        toast_show: true
+      });
+
+    if (this.state.num_of_clicks >= 10)
+      this.setState(() => ({
+        confetti: !this.state.confetti,
+        gravity: 0.25
+      }));
   }
 
   closeToast() {
@@ -47,34 +57,43 @@ export class Intro extends React.Component {
     });
   }
 
+  startConfetti() {
+    setTimeout(() => {
+      this.setState(() => ({
+        dict_check: 3,
+        num_of_clicks: this.state.num_of_clicks + 1,
+        toast_show: true
+      }));
+    }, 3000);
+  }
+
   render() {
     return (
       <div className="intro-text">
         <p className="p-hello">Hello, I'm</p>
 
-        {this.state.dict_check === 1 && (
+        {(this.state.dict_check === 1 || this.state.dict_check === 3) && (
           <h1 onClick={this.dontPressMe}>Avishkar Kolahalu.</h1>
         )}
 
+        {this.state.dict_check === 2 && <h1>Avishkar Kolahalu.</h1>}
         {this.state.dict_check === 2 && (
-          <h1>
-            <a
-              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="rolled-link"
-              onClick={() =>
-                this.setState({
-                  dict_check: 3
-                })
-              }
-            >
-              Avishkar Kolahalu.
-            </a>
-          </h1>
-        )}
+            <DontPressMe
+              closeit={this.closeToast.bind(this)}
+              toast_show={this.state.toast_show}
+              press_dictionary={this.press_dictionary}
+              num_of_clicks={this.state.num_of_clicks}
+            />
+          ) &&
+          this.startConfetti()}
 
-        {this.state.dict_check === 3 && <h1>Avishkar Kolahalu.</h1>}
+        {this.state.dict_check === 3 && (
+          <Confetti
+            style={{ position: "fixed" }}
+            recycle={this.state.confetti}
+            gravity={this.state.gravity}
+          />
+        )}
 
         <p className="p-sd">&#47;&#47; Software Developer</p>
         <DontPressMe
